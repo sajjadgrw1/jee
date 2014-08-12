9d91345a27735674c8dbff56de72888f1bdbb472
sajjad <sajjad@sajjad-PC>
2014-08-12 17:38:00 +0500

    Application Context Event Handling Test

--- springTest/src/springTest/HelloWorld.java
+++ springTest/src/springTest/HelloWorld.java
@@ -1,5 +1,5 @@
 package springTest;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloWorld {
@@ -28,9 +28,11 @@ public class HelloWorld {
 	
 	public static void main (String args[])
 	{
-		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
-		HelloWorld hello = (HelloWorld) context.getBean("helloWorld");
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 		
+		HelloWorld hello = (HelloWorld) context.getBean("helloWorld");
+		context.start();
+		context.stop();
 		System.out.println(hello.getHello());
 
 	}
